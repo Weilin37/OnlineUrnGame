34fa0245d19ba99fdb6f4aee68aae76e27c8d3ac
Weilin Meng <wm84@cornell.edu>
2021-04-22 14:45:51 -0400

    asd

--- app/client/src/js/components/Game.js
+++ app/client/src/js/components/Game.js
@@ -48,6 +48,8 @@ export const Game = () => {
                     </Grid>
                     <Grid item align="center" xs={6} >
                         <p>Room: {gameState.data[0]['room']}</p>
+                        <p>You are: {gameState.player}</p>
+                        <p>Your Alias: {gameState.alias</p>}
                         <p>Current Round: {gameState.data[gameState.data.length - 1]['round']}</p>
                         <p>Player 1 Online: {gameState.data[0]['player1_online'].toString()}</p>
                         <p>Player 2 Online: {gameState.data[0]['player2_online'].toString()}</p>
