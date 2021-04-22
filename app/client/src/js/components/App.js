28b2b2a4053db213bb4fb608ec5c144cffa27cc1
Weilin Meng <wm84@cornell.edu>
2021-04-22 14:26:18 -0400

    added online check to game

--- app/client/src/js/components/App.js
+++ app/client/src/js/components/App.js
@@ -11,14 +11,28 @@ const App = () => {
     const gameState = useSelector(state => state.game);
 
     if (gameState.game_created === true && gameState.game_waiting === false) {
-        return(
-            <Grid container justify="center" alignItems="center" spacing={2}>
-                <NavBar />
-                <Grid item align="center" xs={12} >
-                    <Game />
-                </Grid>
-            </Grid>
-        )
+        if (gameState.data.length > 0) {
+            if (gameState.data[0]['player1_online'] && gameState.data[0]['player2_online']) {
+                return(
+                    <Grid container justify="center" alignItems="center" spacing={2}>
+                        <NavBar />
+                        <Grid item align="center" xs={12} >
+                            <Game />
+                        </Grid>
+                    </Grid>
+                )
+            } else {
+                return(
+                    <Grid container justify="center" alignItems="center" spacing={2}>
+                        <NavBar />
+                        <Grid item align="center" xs={12} >
+                            Not all players are online
+                        </Grid>
+                    </Grid>
+                )
+            }
+
+        }
     } else {
         return(
             <Grid container justify="center" alignItems="center" spacing={2}>
