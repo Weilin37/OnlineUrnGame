7ae2183886f3e7f5dd33ba3dfe9150c51eb5dcd4
Weilin Meng <wm84@cornell.edu>
2021-04-26 12:13:27 -0400

    hf

--- app/server/main/routes.js
+++ app/server/main/routes.js
@@ -230,7 +230,8 @@ router.get('/api/get/readgame', (req,res,next) => {
             false
         END as player2_online
          from public.game_state
-         where room = '${req.query.room}'`,
+         where room = '${req.query.room}'
+         ORDER BY round asc`,
             (q_err, q_res) => {
                 res.json(q_res.rows)
     })
