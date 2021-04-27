d4d6a939c8151190355330429d206e492f5478ea
Weilin Meng <wm84@cornell.edu>
2021-04-27 15:18:15 -0400

    hf

--- app/client/src/js/components/Game.js
+++ app/client/src/js/components/Game.js
@@ -51,6 +51,9 @@ export const Game = () => {
     // render component
     if (gameState.data.length > 0) {
         var tabledata = gameState.data;
+        console.log(tabledata.configurable)
+        Object.defineProperties(tabledata, {configurable: true});
+        console.log(tabledata.configurable)
 
         console.log(tabledata);
         tabledata.forEach(function(v) {
