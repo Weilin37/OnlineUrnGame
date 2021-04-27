e2d6ee3ec24db9c49c672c40870dcf6a20192ae4
Weilin Meng <wm84@cornell.edu>
2021-04-27 14:31:53 -0400

    css

--- app/client/src/js/components/NavBar.js
+++ app/client/src/js/components/NavBar.js
@@ -2,6 +2,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { useSelector } from "react-redux";
+import Divider from '@material-ui/core/Divider';
 
 const NavBar = props => {
     const gameState = useSelector(state => state.game);
@@ -14,12 +15,15 @@ const NavBar = props => {
                 <Typography variant="h6" color="inherit">
                   Urn Game
                 </Typography>
+                <Divider orientation='vertical'/>
                 <Typography variant="h6" color="inherit">
                   Room code (write this down): {gameState.room}
                 </Typography>
+                <Divider orientation='vertical'/>
                 <Typography variant="h6" color="inherit">
                   Alias (write this down): {gameState.alias}
                 </Typography>
+                <Divider orientation='vertical'/>
                 <Typography variant="h6" color="inherit">
                   You are: {gameState.player}
                 </Typography>
