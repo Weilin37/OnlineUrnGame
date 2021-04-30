adb741db937d2ad64b7f71a01b18df340c265f57
Weilin Meng <wm84@cornell.edu>
2021-04-30 14:16:15 -0400

    hf

--- app/client/src/js/components/Instructions.js
+++ app/client/src/js/components/Instructions.js
@@ -142,6 +142,11 @@ const Instructions = () => {
 
                     </Grid>
                     <Grid item align="center" xs={8} >
+                        <button type="button" onClick={handleInstructionsClose}>
+                            Close Instructions
+                        </button>
+                    </Grid>
+                    <Grid item align="center" xs={8} >
                         <Pagination page={instructionsPage} onChange={handleInstructionsPageChange} count={3} />
                     </Grid>
                 </Grid>
@@ -186,9 +191,6 @@ const Instructions = () => {
                         <p>Your earnings will depend on various factors including your decisions,
                         the decisions of the other Player,
                         and how many blue balls were drawn from Player 2’s urns at the end of the round.</p>
-                    </Grid>
-                    <Grid item align="center" xs={8} >
-                        <Pagination page={instructionsPage} onChange={handleInstructionsPageChange} count={3} />
                     </Grid>
                     <Grid item align="center" xs={8} >
                         <Pagination page={instructionsPage} onChange={handleInstructionsPageChange} count={3} />
